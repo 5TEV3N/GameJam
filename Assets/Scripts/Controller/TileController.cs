ff1de7b8311dfde4cdb4a766457ddd3e1f209285
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2016-10-21 17:40:34 -0400

    First Ever Commit

---
+++ Assets/Scripts/Controller/TileController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileController : MonoBehaviour
+{
+    public bool isGreen;
+    public bool isRed;
+
+    public Material greenMat;
+    public Material redMat;
+
+    private Renderer tileRend;
+
+    void Awake()
+    {
+        tileRend = GetComponent<MeshRenderer>();
+    }
+
+    void OnMouseDown()
+    {
+        if (gameObject.tag == "T_Green")
+        {
+            tileRend.material = redMat;
+        }
+
+        if (gameObject.tag == "T_Red")
+        {
+            tileRend.material = greenMat;
+        }
+    }
+}
+/*
+ *         if (isGreen == true && gameObject.tag == "T_Green")
+        {
+            tileRend.material = redMat;
+            isGreen = false;
+        }
+
+        if (isRed == true && gameObject.tag == "T_Red")
+        {
+            tileRend.material = greenMat;
+            isRed = false;
+        }
+
+
+        if (isGreen == false && gameObject.tag == "T_Green")
+        {
+            tileRend.material = greenMat;
+            isGreen = true;
+        }
+
+        if (isRed == false && gameObject.tag == "T_Red")
+        {
+            tileRend.material = redMat;
+            isRed = true;
+        }
+ */
---
+++ Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour 
+{
+    TileController tileController;
+
+    void Awake()
+    {
+
+    }
+}
