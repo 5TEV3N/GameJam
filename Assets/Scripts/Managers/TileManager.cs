3c830f7a5e8594559384b8986c49b4ec1851a15d
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2016-10-21 20:39:17 -0400

    Finish win condition

--- Assets/Scripts/Managers/TileManager.cs
+++ Assets/Scripts/Managers/TileManager.cs
@@ -6,25 +6,41 @@ using System.Collections.Generic;
 public class TileManager : MonoBehaviour
 {
     // http://answers.unity3d.com/questions/141847/removing-objects-from-a-list-in-c.html
+    [Header ("Lists")]
     public List<GameObject> redTilesList = new List <GameObject>();
     public List<GameObject> greenTilesList = new List <GameObject>();
 
+    [Header("")]
+    public int countRedTilesLength;
+    public int countGreenTilesLength;
+
+    public bool redGoal;
+    public bool greenGoal;
+
+    void Update()
+    {
+        countRedTilesLength = redTilesList.Count;
+        countGreenTilesLength = greenTilesList.Count;
+
+        CorrecTileContainer();
+    }
+
     public void CorrecTileContainer()
     {
+        if (greenGoal == true)
+        {
+            if (countRedTilesLength == 0)
+            {
+                print("green win");
+            }
+        }
 
+        if (redGoal == true)
+        {
+            if (countGreenTilesLength == 0)
+            {
+                print("red win");
+            }
+        }
     }
 }
-/*
-public GameObject[] redTileArray;
-public GameObject[] greenTileArray;
-public int goalGreen;
-public int goalRed;
-
-void Awake()
-{
-    redTileArray = GameObject.FindGameObjectsWithTag("T_Red");
-    greenTileArray = GameObject.FindGameObjectsWithTag("T_Green");
-    goalGreen = greenTileArray.Length;
-    goalRed = redTileArray.Length;
-}
-*/
